64ba30845ddb8be3aea6346b5ae92d15964dfe4a
AlexBilla <63157581+AlexBilla02@users.noreply.github.com>
2025-04-12 21:29:41 +0200

    concluso es 5 con semafori

--- esercizio5.c
+++ esercizio5.c
@@ -8,67 +8,131 @@
 
 
 #define N 5
+//#define COND
+#define SEM
+#ifdef COND
+    struct urna_g{
+        pthread_mutex_t mutex;
+        pthread_cond_t cond;
 
-struct urna_g{
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
+        int voti_0, voti_1,vincitore;
+        bool sbloccato;
+        int bloccati,attivi;
 
-    int voti_0, voti_1,vincitore;
-    bool sbloccato;
-    int bloccati,attivi;
+    }urna;
 
-}urna;
+    void init_urna(struct urna_g *urna){
+        pthread_mutexattr_t m;
+        pthread_condattr_t c;
 
-void init_urna(struct urna_g *urna){
-    pthread_mutexattr_t m;
-    pthread_condattr_t c;
+        pthread_mutexattr_init(&m);
+        pthread_mutex_init(&urna->mutex, &m);
+        pthread_mutexattr_destroy(&m);
 
-    pthread_mutexattr_init(&m);
-    pthread_mutex_init(&urna->mutex, &m);
-    pthread_mutexattr_destroy(&m);
+        pthread_condattr_init(&c);
+        pthread_cond_init(&urna->cond, &c);
 
-    pthread_condattr_init(&c);
-    pthread_cond_init(&urna->cond, &c);
+        pthread_condattr_destroy(&c);
 
-    pthread_condattr_destroy(&c);
-
-    urna->voti_0=urna->voti_1=urna->bloccati=urna->attivi=0;
-    urna->sbloccato=false;
-    urna->vincitore=-1;
-}
-
-void vota(struct urna_g *urna, int voto){
-    pthread_mutex_lock(&urna->mutex);
-    if(voto==0)
-        urna->voti_0++;
-    else
-        urna->voti_1++;
-    printf("voti a 0: %d \t voti a 1: %d \n",urna->voti_0,urna->voti_1);
-    if(urna->voti_0>=(N/2+1)|| urna->voti_1>=(N/2+1) && urna->sbloccato==false){
-        urna->sbloccato=true;
-        printf("Sblocco risultato\n");
-        pthread_cond_broadcast(&urna->cond);
-    }
-    pthread_mutex_unlock(&urna->mutex);
-}
-
-
-int risultato(struct urna_g *urna){
-    pthread_mutex_lock(&urna->mutex);
-    while(urna->sbloccato==false){
-        urna->bloccati++;
-        pthread_cond_wait(&urna->cond,&urna->mutex);
-        urna->bloccati--;
+        urna->voti_0=urna->voti_1=urna->bloccati=urna->attivi=0;
+        urna->sbloccato=false;
+        urna->vincitore=-1;
     }
 
-    urna->attivi++;
-    if (urna->voti_0>urna->voti_1)
-        urna->vincitore=0;
-    else 
-        urna ->vincitore=1;
-    pthread_mutex_unlock(&urna->mutex);
-    return urna->vincitore;
-}
+    void vota(struct urna_g *urna, int voto){
+        pthread_mutex_lock(&urna->mutex);
+        if(voto==0)
+            urna->voti_0++;
+        else
+            urna->voti_1++;
+        printf("voti a 0: %d \t voti a 1: %d \n",urna->voti_0,urna->voti_1);
+        if(urna->voti_0>=(N/2+1)|| urna->voti_1>=(N/2+1) && urna->sbloccato==false){
+            urna->sbloccato=true;
+            printf("Sblocco risultato\n");
+            pthread_cond_broadcast(&urna->cond);
+        }
+        pthread_mutex_unlock(&urna->mutex);
+    }
+
+
+    int risultato(struct urna_g *urna){
+        pthread_mutex_lock(&urna->mutex);
+        while(urna->sbloccato==false){
+            printf("Mi blocco...\n");
+            urna->bloccati++;
+            pthread_cond_wait(&urna->cond,&urna->mutex);
+            urna->bloccati--;
+        }
+
+        urna->attivi++;
+        if (urna->voti_0>urna->voti_1)
+            urna->vincitore=0;
+        else 
+            urna ->vincitore=1;
+        pthread_mutex_unlock(&urna->mutex);
+        return urna->vincitore;
+    }
+
+#elif defined SEM
+    struct urna_g{
+        sem_t mutex;
+        sem_t sem;
+        int voti_0, voti_1,vincitore;
+        bool sbloccato;
+        int bloccati,attivi;
+
+    }urna;
+
+    void init_urna(struct urna_g *urna){
+        sem_init(&urna->mutex,0,1);
+        sem_init(&urna->sem,0,0);
+        urna->voti_0=urna->voti_1=urna->bloccati=urna->attivi=0;
+        urna->sbloccato=false;
+        urna->vincitore=-1;
+    }
+
+    void vota(struct urna_g *urna,int voto){
+        sem_wait(&urna->mutex);
+        if(voto==0)
+            urna->voti_0++;
+        else
+            urna->voti_1++;
+        printf("voti a 0: %d \t voti a 1: %d \n",urna->voti_0,urna->voti_1);
+        if(urna->voti_0 >= (N/2+1) || urna->voti_1 >=(N/2+1) && urna->sbloccato==false){
+            urna->sbloccato=true;
+            printf("Sblocco i thread\n");
+            while(urna->bloccati){
+                sem_post(&urna->sem);
+                urna->bloccati--;
+                urna->attivi++;
+            }
+
+        }
+
+        sem_post(&urna->mutex);
+    }
+
+    int risultato(struct urna_g *urna){
+        sem_wait(&urna->mutex);
+        if(urna->sbloccato==true){
+            urna->bloccati--;
+            sem_post(&urna->sem);
+        }
+        else
+            urna->bloccati++;
+        sem_post(&urna->mutex);
+        sem_wait(&urna->sem);
+        if(urna->voti_0>urna->voti_1)
+            urna->vincitore=0;
+        else
+            urna->vincitore=1;
+        return urna->vincitore;
+    }
+
+
+
+
+#endif
 
 void reset_voti(struct urna_g *urna){
     urna->attivi=0;
@@ -78,7 +142,6 @@ void reset_voti(struct urna_g *urna){
     urna->voti_0=0;
     urna->voti_1=0;
 }
-
 void pausetta(void)
 {
   struct timespec t;
@@ -93,10 +156,11 @@ void *thread(void *arg) {
 
     int voto=rand()%2;
     vota(&urna,voto);
+    //pausetta();
     if(voto==risultato(&urna))
-    printf("Ho vinto! Ho votato %d \n",voto);
+        printf("Ho vinto! Ho votato %d \n",voto);
     else
-    printf("Ho perso! Ho votato %d \n",voto);
+        printf("Ho perso! Ho votato %d \n",voto);
     pthread_exit(0);
     return NULL;
 }
